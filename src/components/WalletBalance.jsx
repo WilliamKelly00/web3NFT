56aabf8618d2d51b6565ce5f18c9df5c46799118
William Kelly <Williammkelly00@gmail.com>
2022-01-23 22:57:43 -0500

    added function to get wallet balance and display to user

--- src/components/WalletBalance.jsx
+++ src/components/WalletBalance.jsx
@@ -5,8 +5,20 @@ function WalletBalance(){
     const [balance, setBalance] = useState();
 
     const getBalance = async () => {
-        
+        const [account] = await window.ethereum.request({method: 'eth_requestAccounts'});
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const balance = await provider.getBalance(account);
+        setBalance(ethers.utils.formatEther(balance));  
     };
+
+    return(
+        <div className="card">
+            <div>
+                <h3>Your Balance: {balance}</h3>
+                <button onClick={() => getBalance()}>Show Balance</button>
+            </div>
+        </div>
+    );
 };
 
 export default WalletBalance;
